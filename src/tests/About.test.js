c026a8295ed18184b8f3e646f28065213ba3ee02
Denis <denisv.daros@gmail.com>
2022-07-21 13:00:37 -0300

    segundo requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import About from '../pages/About';
+
+describe('Teste o componente <About.js />', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const titleH2 = screen.getByRole('heading', { name: /About Pokédex/i });
+    expect(titleH2).toBeInTheDocument();
+  });
+  it('Teste se a página contém a imagem de uma Pokédex:', () => {
+    renderWithRouter(<About />);
+
+    const imgPokeDex = screen.getByAltText('Pokédex');
+    expect(imgPokeDex.src).toContain('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+  });
+});
