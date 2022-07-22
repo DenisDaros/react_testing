cc42021cce0fbe09168a44ba73ab61c19df182c6
Denis <denisv.daros@gmail.com>
2022-07-22 09:06:33 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,13 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const paragraph = screen.getByText(/No favorite pokemon found/i);
+    expect(paragraph).toBeInTheDocument();
+  });
+});
