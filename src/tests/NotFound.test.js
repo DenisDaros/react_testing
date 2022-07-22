2e1180c0f63af2fff57a0b4f9c229cf150c95c43
Denis <denisv.daros@gmail.com>
2022-07-22 09:18:43 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se a página contém o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+
+    const title = screen.getByText(/Page requested not found/i);
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+
+    const imgNotFound = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(imgNotFound.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
